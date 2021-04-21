1083f1b1b036d324fcedcdc5e482c911b8fd550b
Pavel Zapletal <wardreth@gmail.com>
2021-04-21 14:48:36 +0200

    dd

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -4,14 +4,17 @@ export default async (req, res) => {
 	const { db } = await connectToDatabase();
 
 	if (req.method === "GET") {
-		const todos = await db
-			.collection("Todos")
-			.find({})
-			.sort({ metacritic: -1 })
-			.limit(20)
-			.toArray();
+		const todos = await db.collection("Todos").find({}).toArray();
+		// .sort({ metacritic: -1 })
+		// .limit(20)
+		// .toArray();
 
 		res.json(todos);
 		console.log(req.method);
+	}
+
+	if (req.method === "POST") {
+		const todos = await db.collection("Todos").insertOne(req.body);
+		res.json(todos);
 	}
 };
